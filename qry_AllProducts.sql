99f5c5ae31b1929416a364d30fd4bcb9ed9b1231
Danny Thanakrit Boonquarmdee <pb.danny@gmail.com>
2017-07-10 16:47:13 +0700

    Create qry_AllProducts.sql

---
+++ qry_AllProducts.sql
@@ -0,0 +1,13 @@
+SELECT quni_AllProducts_step_1.*, Switch(
+	[Source_Code] IN ('TSO','OSB','OSN','PXC','PXD','SRN','SRS','TCS','E2J'), 'Tele',
+	TRUE,'OSS') AS Channel, IIf([TL_Code] in (SELECT DISTINCT [TL_Code] FROM [Telesales Office]), 1, NULL) AS TS_Office, Switch(
+	Channel = 'OSS' And [TS_Office] = 1,'OSS_Tele',
+	Channel = 'OSS','Direct',
+	Channel = 'Tele','Telesales') AS Channel_Sub, Switch(
+	[Channel_Sub] = 'Direct' And TL_Code Like '4*','Indv_TL',
+	[Channel_Sub] = 'Direct' And TL_Code Like '5*' And Source_Code Like 'OCS','New_Corp_TL',
+	[Channel_Sub] = 'Direct' And TL_Code Like '5*' ,'KeyAccount_TL',
+	[Channel_Sub] = 'Direct' And Source_Code Like 'AXA','AXA',
+	[Channel_Sub] = 'OSS_Tele', 'OSS_Tele', 
+	[Channel_Sub] = 'Telesales', 'Telesales') AS TL_Type
+FROM quni_AllProducts_step_1;
