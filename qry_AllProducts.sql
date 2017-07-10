b50182da9bc47cef628746d5d0da4349f1660a21
Danny Thanakrit Boonquarmdee <pb.danny@gmail.com>
2017-07-11 00:06:33 +0700

    Update qry_AllProducts.sql
    Indentation update

--- qry_AllProducts.sql
+++ qry_AllProducts.sql
@@ -1,9 +1,17 @@
-SELECT quni_AllProducts_step_1.*, Switch(
+SELECT quni_AllProducts_step_1.*, 
+
+Switch(
 	[Source_Code] IN ('TSO','OSB','OSN','PXC','PXD','SRN','SRS','TCS','E2J'), 'Tele',
-	TRUE,'OSS') AS Channel, IIf([TL_Code] in (SELECT DISTINCT [TL_Code] FROM [Telesales Office]), 1, NULL) AS TS_Office, Switch(
+	TRUE,'OSS') AS Channel, 
+	
+IIf([TL_Code] in (SELECT DISTINCT [TL_Code] FROM [Telesales Office]), 1, NULL) AS TS_Office,
+
+Switch(
 	Channel = 'OSS' And [TS_Office] = 1,'OSS_Tele',
 	Channel = 'OSS','Direct',
-	Channel = 'Tele','Telesales') AS Channel_Sub, Switch(
+	Channel = 'Tele','Telesales') AS Channel_Sub, 
+
+Switch(
 	[Channel_Sub] = 'Direct' And TL_Code Like '4*','Indv_TL',
 	[Channel_Sub] = 'Direct' And TL_Code Like '5*' And Source_Code Like 'OCS','New_Corp_TL',
 	[Channel_Sub] = 'Direct' And TL_Code Like '5*' ,'KeyAccount_TL',
