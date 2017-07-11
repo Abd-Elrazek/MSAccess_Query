696f93d1bd1539b3cbfb70dc71b82a051241b195
Danny Thanakrit Boonquarmdee <pb.danny@gmail.com>
2017-07-11 22:46:39 +0700

    Update qry_Productivity_Sales_FlagN_Open_Active.sql

--- qry_Productivity_Sales_FlagN_Open_Active.sql
+++ qry_Productivity_Sales_FlagN_Open_Active.sql
@@ -1,7 +1,7 @@
 SELECT
   A.*, 
   1 AS FlagActive
-FROM qry_Productivity_TL_Sales_FlagN_Open AS A 
+FROM qry_Productivity_All_FlagN_Open AS A 
       LEFT JOIN (SELECT DISTINCT Agent_Code, 
                                  MONTH 
                  FROM quni_ALLProducts_step_1 WHERE Result='A')  AS Z 
