5e36820879909cd20340fd8c07c8132c01133116
Danny Thanakrit Boonquarmdee <pb.danny@gmail.com>
2017-07-11 16:24:19 +0700

    Create qry_Productivity_TL_FlagN_Open_Active.sql

---
+++ qry_Productivity_TL_FlagN_Open_Active.sql
@@ -0,0 +1,20 @@
+SELECT 
+    A.SM, 
+    A.M, 
+    A.AMSup, 
+    A.TL_Code_Type, 
+    A.Agent_Code AS TL_Code, 
+    A.Source_Code, 
+    A.Status, 
+    A.NFlag, 
+    A.MONTH, 
+    A.OpenDate, 
+    A.OpenFlag, 
+    1 AS FlagActive
+FROM qry_Productivity_TL_Sales_FlagN_Open AS A 
+    LEFT JOIN (SELECT DISTINCT TL_Code, 
+                                MONTH 
+               FROM quni_ALLProducts_step_1 
+               WHERE Result='A')  AS Z 
+    ON A.Agent_Code=Z.TL_Code
+WHERE (((A.MONTH)=[Z].[MONTH]));
