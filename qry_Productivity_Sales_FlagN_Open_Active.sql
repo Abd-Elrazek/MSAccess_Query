459b1d0d285a075d38814f7c854fcb5ac79d3583
Danny Thanakrit Boonquarmdee <pb.danny@gmail.com>
2017-07-12 00:00:45 +0700

    Delete qry_Productivity_Sales_FlagN_Open_Active.sql

--- qry_Productivity_Sales_FlagN_Open_Active.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  A.*, 
-  1 AS FlagActive
-FROM qry_Productivity_All_FlagN_Open AS A 
-      LEFT JOIN (SELECT DISTINCT Agent_Code, 
-                                 MONTH 
-                 FROM quni_ALLProducts_step_1 WHERE Result='A')  AS Z 
-       ON A.Agent_Code=Z.Agent_Code
-WHERE A.MONTH=Z.MONTH;
