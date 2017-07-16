93b58f161c5b7d68b1892377993a243f5b28f888
Danny Thanakrit Boonquarmdee <pb.danny@gmail.com>
2017-07-16 12:32:14 +0700

    Update qry_Productivity_TL_FlagN_Open_Active.sql

--- qry_Productivity_TL_FlagN_Open_Active.sql
+++ qry_Productivity_TL_FlagN_Open_Active.sql
@@ -1,20 +1,19 @@
-SELECT 
-    A.SM, 
-    A.M, 
-    A.AMSup, 
-    A.TL_Code_Type, 
-    A.Agent_Code AS TL_Code, 
-    A.Source_Code, 
-    A.Status, 
-    A.NFlag, 
-    A.MONTH, 
-    A.OpenDate, 
-    A.OpenFlag, 
-    1 AS FlagActive
-FROM qry_Productivity_All_FlagN_Open AS A 
-    LEFT JOIN (SELECT DISTINCT TL_Code, 
-                                MONTH 
-               FROM quni_ALLProducts_step_1 
-               WHERE Result='A')  AS Z 
-    ON A.Agent_Code=Z.TL_Code
+SELECT A.SM,
+       A.M,
+       A.AMSup,
+       A.TL_Code_Type,
+       A.Agent_Code AS TL_Code,
+       A.Source_Code,
+       A.Status,
+       A.NFlag,
+       A.MONTH,
+       A.OpenDate,
+       A.OpenFlag,
+       1 AS FlagActive
+FROM qry_DataAllTeam_FlagN_Open AS A
+LEFT JOIN
+  (SELECT DISTINCT TL_Code,
+                   MONTH
+   FROM quni_CC_RL_2016_2017_Performance
+   WHERE RESULT='A') AS Z ON A.Agent_Code=Z.TL_Code
 WHERE (((A.MONTH)=[Z].[MONTH]));
